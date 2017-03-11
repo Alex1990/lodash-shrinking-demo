d5b137200b497c1a8a3fefec46ce772921812260
Alex Chao <alexchao1990@gmail.com>
2017-03-12 02:02:11 +0800

    Add fp

--- entry.js
+++ entry.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import sortBy from 'lodash/fp/sortBy';
+import map from 'lodash/fp/map';
 
 const users = [
   {
@@ -27,3 +29,25 @@ console.log(fullNames);
 
 console.log(_.get(users, '0.name.first', ''));
 
+// Functional programming
+
+const data = [
+  {
+    name: 'A',
+    level: 0,
+  },
+  {
+    name: 'B',
+    level: 2,
+  },
+  {
+    name: 'C',
+    level: 1,
+  },
+];
+const getNames = _.flow([
+  sortBy(v => v.level),
+  map(v => v.name)
+]);
+const names = getNames(data);
+console.log(names);
